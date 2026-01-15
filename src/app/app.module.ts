fe6e847de5d448379b5f11a8f8d2384b914da109
oussama <oussamanouhar0@gmail.com>
2026-01-15 11:34:30 +0100

    feat(app): configure main routing and app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './features/products/products.module';
+import { CartsModule } from './features/carts/carts.module';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ProductsModule,
+    CartsModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(
+      withInterceptors([errorInterceptor])
+    )
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
